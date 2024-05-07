4a06489336f6e8832e056c642a996c8a9a41624d
rmbrg <fallturm.bremen@gmail.com>
2024-05-07 11:38:52 +0300

    Pure idiotism

--- user/src/driver.rs
+++ user/src/driver.rs
@@ -3,10 +3,12 @@ use wdk_sys::{
     ULONG,
     FILE_DEVICE_UNKNOWN,
     METHOD_BUFFERED,
-}
+};
 use std::os::windows::raw::HANDLE;
 use windows_kernel_sys::base::DRIVER_OBJECT;
 use winapi::um::winioctl::{FILE_SPECIAL_ACCESS, CTL_CODE}; // == file_any_access
+use Win32::IO::DeviceIoControl;
+//use winapi::shared::minwindef::DWORD;
 
 pub mod driver {
     #[repr(C)]
@@ -17,6 +19,9 @@ pub mod driver {
         pub size: usize,              //SIZE_T
         pub return_size: usize,     //SIZE_T
     }
+    impl Request {
+        fn new {}
+    }
     pub mod codes {
         pub const ATTACH: ULONG = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
         pub const READ: ULONG = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
@@ -26,6 +31,12 @@ pub mod driver {
     pub unsafe extern "C" fn CTL_CODE () {
         // TODO:
         // no need?
+    }
+
+    pub fn ATTACH(driver_handle: HANDLE, pid: u32) -> bool {
+        let req = Request {pid: u32 as *HANDLE};
+
+        return DeviceIoControl(&driver_handle, )
     }
 
 }
